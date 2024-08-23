75535a5e58bad8e0c71ea2ba33d7015cd3613fb7
Evbgsl <evbgsl@gmail.com>
2024-08-23 21:32:51 +0300

    Added types for Header function

---
+++ js/widgets/Header/types.ts
@@ -0,0 +1,8 @@
+import { NavItemData } from '../../features/Navigation/types.js';
+import { LangItemData } from '../../features/Lang/types.js';
+
+
+export type HeaderData = {
+  langs: LangItemData[];
+  navigation: NavItemData[];
+}
