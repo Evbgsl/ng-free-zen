6225bf49e9d9e8bf17431a843d5936030c57d531
Evbgsl <evbgsl@gmail.com>
2024-08-23 21:35:03 +0300

    Added features for Header component

---
+++ js/features/Theme/Theme.js
@@ -0,0 +1,16 @@
+import { ThemeLogo } from "../../ui/ThemeLogo/index.js"
+
+/**
+ * @function Theme
+ * @param {string} className
+ * @returns {string} HTML
+ */
+
+export const Theme = (className) => `
+  <button class="${className}"
+    type="button"
+    id="theme"
+  >
+    ${ThemeLogo()}
+  </button>
+`
