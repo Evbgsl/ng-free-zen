65f145895120c20ce6fcae128d98fe654e604828
Evbgsl <evbgsl@gmail.com>
2024-08-26 07:30:05 +0300

    Code refactored. Added Warranty component

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 import { data } from './data.js';
 import { Header } from './widgets/Header/index.js';
 import { Download } from './widgets/Download/index.js';
+import { Warranty } from './widgets/Warranty/index.js';
 
 const $root = document.querySelector('#root');
 
 $root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
 $root?.insertAdjacentHTML('beforeend', Download(data.download));
+$root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
