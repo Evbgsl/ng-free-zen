1e262ce2f4abea102073f4ee3c2a278bbb779869
Evbgsl <evbgsl@gmail.com>
2024-08-26 11:32:52 +0300

    WIP

---
+++ js/widgets/Cashback/types.ts
@@ -0,0 +1,8 @@
+import { TitleData } from '../../ui/Title/types';
+import { ButtonData } from '../../ui/Button/types';
+
+export type CashbackData = {
+  title: TitleData;
+  texts: string[];
+  button: ButtonData;
+};
