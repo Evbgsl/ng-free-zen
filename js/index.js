a8629c736a3c8b42361fab8dc3071cf36b0549b5
Evbgsl <evbgsl@gmail.com>
2024-08-26 10:11:40 +0300

    Code refactored. Added Care component

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ import { data } from './data.js';
 import { Header } from './widgets/Header/index.js';
 import { Download } from './widgets/Download/index.js';
 import { Warranty } from './widgets/Warranty/index.js';
+import { Care } from './widgets/Care/index.js';
 
 const $root = document.querySelector('#root');
 
 $root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
 $root?.insertAdjacentHTML('beforeend', Download(data.download));
 $root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
+$root?.insertAdjacentHTML('beforeend', Care(data.care));
