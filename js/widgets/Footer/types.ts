805c74ec1eae14c7608f99fca5af02eefd101eb1
Evbgsl <evbgsl@gmail.com>
2024-08-27 16:38:24 +0300

    Added types for Footer

---
+++ js/widgets/Footer/types.ts
@@ -0,0 +1,8 @@
+import { CategoriesData } from '../../ui/Column/types'
+import { DeveloperData } from '../../ui/FooterDev/types'
+
+export type FooterData = {
+  categories: CategoriesData[];
+  texts: string[];
+  developer: DeveloperData;
+}
