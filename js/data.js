bd5ef5fd8a891bfc1986a30c9b27f8666c9307c8
Evbgsl <evbgsl@gmail.com>
2024-08-27 16:39:05 +0300

    Code refactored. Added data's for Footer

--- js/data.js
+++ js/data.js
@@ -273,12 +273,12 @@ export const data = {
       },
     ],
     texts: [
-      'Some paragraph 1',
-      'Some paragraph 2',
-      'Some paragraph 3',
-      'Some paragraph 4',
-      'Some paragraph 5',
-      'Some paragraph 6',
+      'ZEN is a licensed financial institution under the supervision of the Central Bank of Lithuania approved by European Banking Authority for 31 countries.',
+      'Registration number of the company 304749651, VAT ID LT100011714916.',
+      'License of electronic money institutions number LB000457.',
+      'Share capital 2.185.000,00 EUR.',
+      'Company address: Mėsinių g. 5, LT-01133, Vilnius, Lithuania.',
+      'Sales Office: ul. Emilii Plater 53, 00-113 Warsaw, Poland.',
     ],
     developer: {
       url: 'https://example.com',
