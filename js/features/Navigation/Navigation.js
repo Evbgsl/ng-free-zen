2032bf8a703623400b6b9f078dc775ff3104127e
Evbgsl <evbgsl@gmail.com>
2024-09-05 19:15:24 +0300

    WIP

--- js/features/Navigation/Navigation.js
+++ js/features/Navigation/Navigation.js
@@ -12,7 +12,7 @@ export const Navigation = (navList) => `
   <ul class="nav" id="nav">
     ${navList.map((navItem) => `
       <li class="nav__item">
-        <a href="${navItem.key}">
+        <a href="#${navItem.key}">
           ${navItem.name}
         </a>
       </li>
