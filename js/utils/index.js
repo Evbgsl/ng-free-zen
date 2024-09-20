822f8c892364119a7b17899680ac0e1c3bd62c8d
Evbgsl <evbgsl@gmail.com>
2024-09-20 13:50:08 +0300

    Code refactored

--- js/utils/index.js
+++ js/utils/index.js
@@ -1,7 +1,9 @@
-export { toggleBurgerAndNav } from './helpers/toggleBurgerAndNav.js';
 export { getData } from './helpers/getData.js';
 export { renderApp } from './helpers/renderApp.js'
 export { processDataAndRender } from './helpers/processDataAndRender.js';
+
 export { showLoader } from './preLoader/showLoader.js';
 export { hideLoader } from './preLoader/hideLoader.js';
+
 export { addHandlers } from './handlers/addHandlers.js';
+export { toggleBurgerAndNav } from './helpers/toggleBurgerAndNav.js';
