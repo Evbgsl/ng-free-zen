b5ed7f4896335f24c3ac353e174cebe3a649de6b
Evbgsl <evbgsl@gmail.com>
2024-09-20 13:00:56 +0300

    Code refactored

---
+++ js/utils/handlers/addHandlers.js
@@ -0,0 +1,37 @@
+import {
+  handleLogoClick,
+  onThemeClick,
+  handleBurgerClick,
+  handleNavLinkClick,
+  handleOrderClick,
+  handleCloseClick,
+  handleLangChange,
+} from './handlers.js';
+
+/**
+ * @typedef {import ('../../types').AppDataFromAPI} AppData
+ */
+
+/**
+ * @function addHandlers
+ * @param {AppData} data
+ */
+
+export const addHandlers = (data) => {
+  const $logoBtn = document.querySelector('#logo');
+  const $themeBtn = document.querySelector('#theme');
+  const $burgerBtn = document.querySelector('#burger');
+  const $navLinks = document.querySelector('#nav');
+  const $orderBtn = document.querySelector('#order-open');
+  const $closeBtn = document.querySelector('#close');
+  const $langSelector = document.querySelector('#lang');
+
+  $logoBtn?.addEventListener('click', handleLogoClick);
+  $themeBtn?.addEventListener('click', (event) =>
+    onThemeClick(event, data.clients.brands));
+  $burgerBtn?.addEventListener('click', handleBurgerClick);
+  $navLinks?.addEventListener('click', handleNavLinkClick);
+  $orderBtn?.addEventListener('click', handleOrderClick);
+  $closeBtn?.addEventListener('click', handleCloseClick);
+  $langSelector?.addEventListener('change', handleLangChange);
+};
